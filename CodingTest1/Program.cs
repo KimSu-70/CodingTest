a07d4c5ee0878b41c279ef97af7b47474c8637da
KimSu-70 <rlatngh4770@naver.com>
2024-08-05 16:37:12 +0900

    1번 문제 풀이

---
+++ CodingTest1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CodingTest1
+{
+    //2024년 08월 05일
+    //문제 : 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+    //URL : https://school.programmers.co.kr/learn/courses/30/lessons/120802
+
+    public class CodingTest1
+    {
+        public class Solution
+        {
+            public int solution(int num1, int num2)
+            {
+                int answer = num1 + num2;
+                return answer;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int a = s.solution(5, 8);
+            Console.WriteLine(a);
+        }
+    }
+}
