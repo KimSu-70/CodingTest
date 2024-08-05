625b99263e487886e9735f176cdb088becf9c02e
KimSu-70 <rlatngh4770@naver.com>
2024-08-05 16:54:24 +0900

    입문 2번 문제 풀이

---
+++ CodingTest2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CodingTest2
+{
+    //2024년 08월 05일
+    //문제 : 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+    //URL : https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+    internal class Program
+    {
+        public class Solution
+        {
+            public int solution(int num1, int num2)
+            {
+                int answer = num1 - num2;
+                return answer;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int a = s.solution(100, 2);
+            Console.WriteLine(a);
+        }
+    }
+}
